fefdea21c057284ee611b3361631e17b68d1c064
tolbarweb <tolbarweb@gmail.com>
2020-08-08 17:11:43 -0500

    modal eliminar

---
+++ src/app/modulos/modal-confirmar-eliminar/modal-confirmar-eliminar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-confirmar-eliminar',
+  templateUrl: './modal-confirmar-eliminar.component.html',
+  styleUrls: ['./modal-confirmar-eliminar.component.css']
+})
+export class ModalConfirmarEliminarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
